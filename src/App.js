8ab64d3bea0faa4a767185539518cf3295e39f69
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-12 00:39:16 +0100

    header-part1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Component/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>lets build a linkedin clone</h1>
+      <Header />
     </div>
   );
 }
