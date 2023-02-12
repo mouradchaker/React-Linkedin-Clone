c57955f142e76e393a8567e17b81ffb99a24b3f2
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-13 00:29:01 +0100

    headerPart2

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HeaderOption from "../HeaderOption/HeaderOption";
+import img from "./profil.jpg";
 
 function Header() {
   return (
@@ -17,7 +24,14 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar={img} title="Me" />
+      </div>
     </div>
   );
 }
