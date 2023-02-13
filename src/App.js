ee4184592273ff8011da4d835b3f577bd4bf502a
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-02-13 23:04:14 +0100

    sidebarPart1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Component/Header/Header";
+import Sidebar from "./Component/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,34 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img src="" alt="" />
+        <Avatar className="sidebar__avatar" />
+        <h2>Mourad Chaker</h2>
+        <h4>mouradchaker61@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">3,543</p>
+        </div>
+
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,193</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
